b1ddb81e971bd09d61f27b66a90193cd60540159
RobLR <94964242+RobLR@users.noreply.github.com>
2022-12-05 10:54:24 +0000

    Extra functionaity

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -4,12 +4,41 @@ import { Button } from './Button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
-  component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
+	title: 'Example/Button',
+	component: Button,
+	// More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+	argTypes: {
+		backgroundColor: { control: 'color' },
+
+		label: {
+			description: 'overwritten description',
+			table: {
+				type: {
+					summary: 'something short',
+					detail: 'something really really long',
+				},
+			},
+			control: {
+				type: null,
+			},
+		},
+	},
+	// decorators: [
+	// 	(Story) => (
+	// 		<div style={{ margin: '3em' }}>
+	// 			<Story />
+	// 		</div>
+	// 	),
+	// ],
+
+	// If using Styled Components to get a theme
+	// decorators = [
+	//     (Story) => (
+	//       <ThemeProvider theme="default">
+	//         <Story />
+	//       </ThemeProvider>
+	//     ),
+	//   ]
 };
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
@@ -18,23 +47,28 @@ const Template = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
-  primary: true,
-  label: 'Button',
+	primary: true,
+	label: 'Button',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  label: 'Button',
+	label: 'Button',
 };
 
 export const Large = Template.bind({});
 Large.args = {
-  size: 'large',
-  label: 'Button',
+	size: 'large',
+	label: 'Button',
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  size: 'small',
-  label: 'Button',
+	size: 'small',
+	label: 'Button',
 };
+
+//Centers the story component on the canvas
+// Small.parameters = {
+// 	layout: 'centered',
+// };
--- src/stories/Page.stories.jsx
+++ src/stories/Page.stories.jsx
@@ -4,12 +4,12 @@ import { within, userEvent } from '@storybook/testing-library';
 import { Page } from './Page';
 
 export default {
-  title: 'Example/Page',
-  component: Page,
-  parameters: {
-    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
-    layout: 'fullscreen',
-  },
+	title: 'Example/Page',
+	component: Page,
+	parameters: {
+		// More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+		layout: 'fullscreen',
+	},
 };
 
 const Template = (args) => <Page {...args} />;
@@ -17,9 +17,10 @@ const Template = (args) => <Page {...args} />;
 // More on interaction testing: https://storybook.js.org/docs/react/writing-tests/interaction-testing
 export const LoggedOut = Template.bind({});
 
+// Doesn't start logged in, it uses the interaction to simulate a user clicking log in
 export const LoggedIn = Template.bind({});
 LoggedIn.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement);
-  const loginButton = await canvas.getByRole('button', { name: /Log in/i });
-  await userEvent.click(loginButton);
+	const canvas = within(canvasElement);
+	const loginButton = await canvas.getByRole('button', { name: /Log in/i });
+	await userEvent.click(loginButton);
 };
